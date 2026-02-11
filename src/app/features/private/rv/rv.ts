23f5d8116cc7bf9422108641a61cfb89306e42e7
Ahmadou Bamba Momar Cissé (KINGABMC) <ahmadou6czac@gmail.com>
2026-02-11 04:44:19 +0000

    mise à jour de la page de gestion des rendez-vous : ajout du statut "Annulé" et du type "Urgence", ainsi que l'ajout de texte aux boutons de pagination.

--- src/app/features/private/rv/rv.ts
+++ src/app/features/private/rv/rv.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterLink} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RvService } from './services/rv-service';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RvFilterModel, RvListeResponse } from './models/rv.model';
+
 @Component({
   selector: 'app-rv',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, FormsModule],
   templateUrl: './rv.html',
   styleUrl: './rv.css',
 })
-export class Rv {
+export class Rv implements OnInit {
+  rvResponse?: RvListeResponse;
+  filter: RvFilterModel = { statut: '', type: '' };
+  title = "Mes Rendez-vous";
 
+  constructor(private rvService: RvService) {}
+
+  ngOnInit() {
+    this.loadRV();
+  }
+
+  loadRV() {
+    this.rvResponse = this.rvService.getRendezVous(this.filter);
+  }
+
+  onFilterChange() {
+    this.loadRV();
+  }
+
+  onPageChange(page: number) {
+    if (this.rvResponse && page >= 1 && page <= this.rvResponse.totalPages) {
+      this.filter.page = page;
+      this.loadRV();
+    }
+  }
 }
